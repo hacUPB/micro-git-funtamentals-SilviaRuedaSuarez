fa6dc9633e60538d653bfe14a90534f41193a13f
silviaruedasuarez <silvia.rueda.s@outlook.es>
2024-02-01 14:02:58 -0500

    segunda linea

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -17,6 +17,15 @@ int main(int argc, char *argv[]) {
     }
     
     /*Implement the function that calculates the factorial of the input argument*/
+    int factorial = 1;
+    int b;
+    for (b = number; b > 1; b--)
+    {
+        factorial *= b;
+    }
+
+    /*Print the result*/
+    printf("El factorial de %d es %d", number, factorial);
     
     /*Print the result*/
     
