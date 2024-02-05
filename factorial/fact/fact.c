d2d60ae0a37542547bb467271f1d1b039c31239a
SilviaRuedaSuarez <silvia.rueda.s@outlook.es>
2024-02-05 18:41:42 -0500

    linea 20- linea 20-

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     }
     
     /*Implement the function that calculates the factorial of the input argument*/
-    int factorial = 10;
+    int factorial = 1;
     int b;
     for (b = number; b > 1; b--)
     {
